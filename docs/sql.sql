fd975274aaa1f3d721217fcfeaab375979aa2c36
magnusLeona <magnusleona@outlook.com>
2021-09-10 09:36:29 +0800

    Insert some new docs :wq

---
+++ docs/sql.sql
@@ -0,0 +1,5 @@
+insert into teacher(name) values("1"),("2")
+
+insert into student(name) values("A")("B")("C")("D")
+
+insert into teacher_student(teacher_id, student_id) values(1, 1), (1, 2), (1,3),(1,4),(2,1),(2,2),(2,3),(2,4)
